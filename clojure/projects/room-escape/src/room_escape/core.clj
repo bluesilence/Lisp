8eb6f44fbda299227c0aafb6a01af5bc07213faf
root <root@Avalon.AvalonOfSunny.h4.internal.cloudapp.net>
2014-06-12 05:29:58 +0000

    Add toggle-visible function

--- clojure/projects/room-escape/src/room_escape/core.clj
+++ clojure/projects/room-escape/src/room_escape/core.clj
@@ -19,66 +19,69 @@
 (defn update-status [key value]
   (swap! current-status assoc key value))
 
-(defn update-room [room-name]
-  (update-status :location [room-name (second (:location @current-status))]))
-
-(defn get-current-room []
-  (first (:location @current-status)))
+(defn update-room [room-id]
+  (update-status :location [room-id (second (:location @current-status))]))
 
 (def rooms [{:id 1
              :name "starting room"
-             :visible 1
-             :description "This is a strange room. Barely no furniture except a [table] and a [door]."
-             :spots [1 2]
+             :description "This is a strange room. Barely no furniture except a [ table ] and a [ door ]."
+             :items [2 3]
              }])
 
-(def starting-room (:name (first rooms)))
+(def starting-room (:id (first rooms)))
 
-(def spots [{:id 1
+(def spots [{:id 2
              :name "table"
-             :visible 0
              :description "This is a small square table. It seems that there is something on the table..."
-             :items [1]}
-            {:id 2
+             :items [4]}
+            {:id 3
              :name "door"
-             :visible 0
              :description "It is a door locked with a password panel. Maybe the password is written somewhere..."
-             :items [2]}])
+             :items [5]}])
 
-(def items [{:id 1
+(def items [{:id 4
              :name "card"
-             :visible 0
              :description "It's a card with number 0428 on it."}
-            {:id 2
+            {:id 5
              :name "password Panel"
-             :visible 0
              :description "There are button 0~9 on the panel. The length of the password seems to be 4."
              :state "Locked"}])
 
 (def objects (vec (concat rooms spots items)))
+(def visible (atom #{}))
+
+(defn visible? [id]
+  (contains? @visible id))
+
+(defn toggle-visible [id]
+  (if (visible? id)
+    (swap! visible disj id)
+    (swap! visible conj id)))
 
 ; To-Do: use macro to generate these locate functions
 (defn locate-by-id [id]
-  (doseq [object objects]
-    (let [object-id (:id object)]
-      (if (= object-id id)
-        object
-        nil))))
+  (first (filter (comp #(= % id) #(:id %)) objects)))
 
 (defn locate-by-name [name]
   (first (filter (comp #(= % name) #(:name %)) objects)))
 
+(defn get-current-room []
+  (when-let [id (first (:location @current-status))]
+    (:name (locate-by-id id))))
+
 (defn display [description]
-  (println ">>" description))
+  (println description))
 
 (defn check [object-name]
   (if (nil? object-name)
     (println "Object name cannot be nil.")
     (if-let [object (locate-by-name object-name)]
-      (if (= (:visible object) 1)
-        (display (:description object))
-        (println "You didn't see any " object-name " around here."))
-      (println "There is no " object-name " around here."))))
+      (if (visible? (:id object))
+        (do (display (:description object))
+            (doseq [item (:items object)]
+              (toggle-visible item)))
+        (println "You didn't see any [" object-name "] around here."))
+      (println "There is no [" object-name "] around here."))))
 
 (defn look-around []
   (display "You looked around.")
@@ -93,9 +96,6 @@
     (display (str "[" (:name action) "] " (:description action)))))
 
 (defn execute [command & args]
-  (println command)
-  (println args)
-  (println (string/join args))
   (let [action (->> action-list (filter (comp #(= (:name %) (str command)))) first :function)
         args-str (string/join args)]
     (if (nil? action)
@@ -107,18 +107,23 @@
 (defn initialize []
   (println starting-message)
   (update-room starting-room)
+  (toggle-visible starting-room)
   (help))
+
+(defn display-prompt []
+  (print ">>")
+  (flush))
 
 (defn -main
   "A text only game to escape from a locked room."
   [& args]
   (initialize)
-  (println "Your action: ")
+  (display-prompt)
   (loop [command-str (str (read-line))]
     (if (= command-str "quit")
       (println "Bye~!")
       (do (when-not (= command-str "")
             (let [command-vector (string/split command-str #" ")]
               (execute (first command-vector) (string/join (rest command-vector)))))
-          (println "Your action: ")
+          (display-prompt)
           (recur (str (read-line)))))))
