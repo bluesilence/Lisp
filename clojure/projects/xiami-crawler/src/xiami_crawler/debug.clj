ff15cd6ffc71b10058a14632e7e0d78d96cc5172
bluesilence <sunny@ubuntu.ubuntu-domain>
2014-06-28 19:55:19 +0800

    Add debug function for get and slurp

--- clojure/projects/xiami-crawler/src/xiami_crawler/debug.clj
+++ clojure/projects/xiami-crawler/src/xiami_crawler/debug.clj
@@ -1,20 +1,30 @@
 (ns xiami-crawler.debug
   (:gen-class))
 
+(require '[clj-http.client :as client])
 (require '[net.cgrand.enlive-html :as enlive])
 (use '[clojure.string :only (lower-case)])
 
 (require '[xiami-crawler.logger :as logger])
 
-(def log->file (logger/file-logger "logs/page.log"))
+(def log-slurp (logger/file-logger "logs/slurp-page.log"))
+(def log-get (logger/file-logger "logs/get-page.log"))
 
 (defn slurp-single-page [url]
   (let [html (enlive/html-resource (java.io.StringReader. (slurp url)))]
-    (log->file html)
+    (log-slurp html)
     (println html)))
 
 (defn test-slurp []
   (slurp-single-page "http://www.xiami.com/album/1"))
+
+(defn get-single-page [url]
+  (let [html (enlive/html-resource (java.io.StringReader. (:body (client/get url))))]
+    (log-get html)
+    (println html)))
+
+(defn test-get []
+  (get-single-page "http://www.xiami.com/album/1"))
 
 (defn match [html pattern]
   (re-seq pattern html))
