aeda2d4ea716a42660c51963fa28570e7de8dee8
bluesilence <sunny@ubuntu.ubuntu-domain>
2014-07-26 10:39:13 +0800

    Use socket server

--- clojure/projects/room-escape/project.clj
+++ clojure/projects/room-escape/project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [server-socket "1.0.0"]]
   :main room-escape.core
   :aot [room-escape.common room-escape.script room-escape.util]
   :target-path "target/%s"
--- clojure/projects/room-escape/src/room_escape/core.clj
+++ clojure/projects/room-escape/src/room_escape/core.clj
@@ -1,6 +1,9 @@
 (ns room-escape.core
   (:gen-class))
 
+(import '[java.io BufferedReader InputStreamReader OutputStreamWriter])
+(use 'server.socket)
+
 (use '[clojure.string :as string])
 (use '[room-escape.script :as script])
 (use '[room-escape common util script])
@@ -43,20 +46,27 @@
   (print ">>")
   (flush))
 
+(defn game-server []
+  (letfn [(echo [in out]
+            (binding [*in* (BufferedReader. (InputStreamReader. in))
+                      *out* (OutputStreamWriter. out)]
+              (initialize)
+              (display-prompt)
+              (loop [command-str (str (read-line))]
+                (do (when-not (= command-str "")
+                      (let [command-vector (string/split command-str #" ")]
+                        (execute (first command-vector) (rest command-vector))))
+                  (if (win?)
+                    (do
+                      (swap! continue not)
+                      (display win-message))
+                    (when (continue?)
+                      (display-prompt)
+                      (recur (str (read-line)))))))
+             (display "Goodbye~!")))]
+    (create-server 8080 echo)))
+
 (defn -main
   "A text only game to escape from a locked room."
   [& args]
-  (initialize)
-  (display-prompt)
-  (loop [command-str (str (read-line))]
-    (do (when-not (= command-str "")
-      (let [command-vector (string/split command-str #" ")]
-        (execute (first command-vector) (rest command-vector))))
-      (if (win?)
-        (do
-          (swap! continue not)
-          (display win-message))
-        (when (continue?)
-          (display-prompt)
-          (recur (str (read-line)))))))
-  (display "Goodbye~!"))
+  (game-server))
