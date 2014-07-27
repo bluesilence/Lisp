238369b0b0c1265273a5a066d95a2991ff548a6d
root <root@Avalon.AvalonOfSunny.h4.internal.cloudapp.net>
2014-07-27 14:57:00 +0000

    Add description

--- clojure/projects/room-escape/project.clj
+++ clojure/projects/room-escape/project.clj
@@ -1,6 +1,7 @@
 (defproject room-escape "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "ONLINE now! Play it right now by:
+                telnet avalonOfSunny.cloudapp.net 8080"
+  :url "telnet avalonOfSunny.cloudapp.net 8080"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
